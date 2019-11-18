3db4e358d61702bc2dab94026f8f78bed4798d3d
Manny George TUN PECH <mtunpech@tibco-support.com>
2019-11-18 14:20:15 -0600

    Added components' index export file

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as FileExplorer } from './file-explorer/file-explorer.component';
+export { default as ParcelsList } from './parcels-list/parcels-list.component';
+export { default as ParcelDetails } from './parcel-details/parcel-details.component';
---
+++ src/components/parcels-list/parcels-list.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ParcelsListComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <p>This is the parcel list</p>;
+    }
+}
+
+export default ParcelsListComponent;
